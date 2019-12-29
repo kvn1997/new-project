e2da92fbffcb32610185c9af71d7446d37ff592f
kvn1997 <57558336+kvn1997@users.noreply.github.com>
2019-12-29 14:54:39 +0100

    Add files via upload

--- progtamma/lalla.js
+++ progtamma/lalla.js
@@ -1,46 +1,52 @@
-var Kevin = {
-    horas: 20,
-    profesion: "pizzero",
-}
-var Johnny = {
-    horas: 40,
-    profesion: "cocinero",
-}
-var Fran = {
-    horas: 16,
-    profesion: "sala",
-}
-var Anna = {
-    horas: 40,
-    profesion: "barra",
-}
-cocina = [];
-pizzeria = [];
-barra = [];
-sala = [];
-
-var empleados = [Kevin, Johnny, Fran, Anna];
-// console.log(empleados[0].horas);
-// cocina.push(empleados.);
-// console.log(cocina);
-var horas = [empleados[0].horas, empleados[1].horas,
-    empleados[2].horas, empleados[3].horas
-];
-var profesion = [empleados[0].profesion, empleados[1].profesion,
-    empleados[2].profesion, empleados[3].profesion
-];
-var semana = [40];
-if (horas[0] < semana) {
-    semana.push(empleados[0]);
-}
-var lunes = [];
-var martes = [];
-var miercoles = [];
-var jueves = [];
-var viernes = [];
-var sabado = [];
-var domingo = [];
 $(document).ready(function() {
+    var Kevin = {
+        horas: 20,
+        profesion: "pizzero",
+    }
+    var Johnny = {
+        horas: 40,
+        profesion: "cocinero",
+    }
+    var Fran = {
+        horas: 16,
+        profesion: "sala",
+    }
+    var Anna = {
+        horas: 40,
+        profesion: "barra",
+    }
+    cocina = [];
+    pizzeria = [];
+    barra = [];
+    sala = [];
+    var empleados = [Kevin, Johnny, Fran, Anna];
+    var horas = [empleados[0].horas, empleados[1].horas,
+        empleados[2].horas, empleados[3].horas
+    ];
+    var profesion = [empleados[0].profesion, empleados[1].profesion,
+        empleados[2].profesion, empleados[3].profesion
+    ];
+    var lunes = [];
+    var martes = [];
+    var miercoles = [];
+    var jueves = [];
+    var viernes = [];
+    var sabado = [];
+    var domingo = [];
+    var hora_Lunes = 4;
+    var hora_Martes = 4;
+    var hora_Miercoles = 4;
+    var hora_Jueves = 4;
+    var hora_Viernes = 4;
+    var hora_Sabado = 4;
+    var hora_Domingo = 4;
+
     $("#dias").css("display", "flex");
+    $("#dias").css("text-align", "center");
+    $("#dias").css("color", "black");
+    $("#dias p").css("color", "blue");
+    $("button").css("width", "70px");
+
+
+
 });
-var input1 = $("#input1");
