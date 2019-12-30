2480d391cf2a8250b0e870b5b1445a6ef1ee275c
kvn1997 <57558336+kvn1997@users.noreply.github.com>
2019-12-30 18:27:30 +0100

    Add files via upload
    lalla

--- progtamma/lalla.js
+++ progtamma/lalla.js
@@ -1,31 +1,83 @@
 $(document).ready(function() {
     var Kevin = {
+        nombre: "Kevin",
         horas: 20,
-        profesion: "pizzero",
+        profesion: "sala",
     }
     var Johnny = {
+        nombre: "Johnny",
         horas: 40,
-        profesion: "cocinero",
+        profesion: "sala",
     }
     var Fran = {
+        nombre: "Fran",
         horas: 16,
         profesion: "sala",
     }
     var Anna = {
+        nombre: "Anna",
         horas: 40,
-        profesion: "barra",
+        profesion: "sala",
+    }
+    var Rosario = {
+        nombre: "Rosario",
+        horas: 20,
+        profesion: "sala",
+    }
+    var Andrea = {
+        nombre: "Andrea",
+        horas: 40,
+        profesion: "sala",
+    }
+    var Juan = {
+        nombre: "Juan",
+        horas: 16,
+        profesion: "sala",
+    }
+    var Lalla = {
+        nombre: "Lalla",
+        horas: 40,
+        profesion: "sala",
+    }
+    var Jimmy = {
+        nombre: "Jimmy",
+        horas: 16,
+        profesion: "sala",
+    }
+    var Olga = {
+        nombre: "Olga",
+        horas: 40,
+        profesion: "sala",
     }
     cocina = [];
     pizzeria = [];
     barra = [];
-    sala = [];
-    var empleados = [Kevin, Johnny, Fran, Anna];
+    sala = [Kevin, Johnny, Anna, Fran, Rosario, Olga, Jimmy, Juan, Andrea, Lalla];
+
+    var empleados = [Kevin, Johnny, Fran, Anna, Olga, Jimmy];
     var horas = [empleados[0].horas, empleados[1].horas,
         empleados[2].horas, empleados[3].horas
     ];
-    var profesion = [empleados[0].profesion, empleados[1].profesion,
-        empleados[2].profesion, empleados[3].profesion
-    ];
+
+    $("#btn-1").click(function() {
+        console.log($("#1").val());
+        var valor = $("#1").val();
+        var random = Math.floor((Math.random() * 5) + 1);
+        if (valor < empleados[random].horas) {
+            console.log(random);
+            console.log(empleados[random].nombre);
+            console.log(empleados[random].horas);
+        }
+
+    });
+
+
+
+
+
+    // var profesion = [empleados[0].profesion, empleados[1].profesion,
+    //     empleados[2].profesion, empleados[3].profesion
+    // ];
     var lunes = [];
     var martes = [];
     var miercoles = [];
@@ -41,12 +93,13 @@ $(document).ready(function() {
     var hora_Sabado = 4;
     var hora_Domingo = 4;
 
-    $("#dias").css("display", "flex");
-    $("#dias").css("text-align", "center");
+    $("#Lunes").css("display", "flex");
+    $("#Martes").css("display", "flex");
+    $("#Miercoles").css("display", "flex");
+    $("#Jueves").css("display", "flex");
     $("#dias").css("color", "black");
     $("#dias p").css("color", "blue");
     $("button").css("width", "70px");
-
 
 
 });
